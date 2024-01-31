545391444c630ecdc0634a5b04544a06815dccd8
Henrique Carvalho <83596538+henriqueoakdev@users.noreply.github.com>
2024-01-31 15:41:02 -0300

    Configuração de router, CSS geral e criação de formulários básicos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import './App.css';
+//CSS
+import "./App.css";
+
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+//pages
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 
 function App() {
   return (
     <div className="App">
-      <h1>Miniblog</h1>
+      <BrowserRouter>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+      </BrowserRouter>
+      <Footer />
     </div>
   );
 }
